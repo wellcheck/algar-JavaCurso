462f0bbefbf1d94b17c44169db174dcf5952b726
Wellington Paixao <wellington.ferreira@IF0793.iw.local>
2021-04-05 20:57:51 -0300

    FIm Polimorfismo

--- Algarworks/src/com/algaworks/curso/fjoo/polimorfismo/TestePolimorfismo.java
+++ Algarworks/src/com/algaworks/curso/fjoo/polimorfismo/TestePolimorfismo.java
@@ -19,9 +19,22 @@ public class TestePolimorfismo {
 		
 	}
 
+	// O instanceOf é usado para saber se uma determinado variavel é de um tipo expecifico. 
 	public static void imprimirSaldo(Conta conta) {
 		System.out.println("Saldo da conta: R$" + conta.getSaldo());
 
+		// Saber se o o bjeto do tipo "conta" é do tipo "ContaCorrente".
+		if (conta instanceof ContaCorrente) {
+			ContaCorrente cc = (ContaCorrente) conta;
+			System.out.println("O limite da cc é: R$" + cc.getLimete() );
+		}
+		
+		// Saber se o objeto do tipo "conta" é do tipo "ContaPoupanca".
+		if (conta instanceof ContaPoupanca) {
+			ContaPoupanca cp = (ContaPoupanca) conta;
+			System.out.println("O rendimento da cp é: R$" + cp.getRendimentos());
+		}
+		
 	}
 
 }
